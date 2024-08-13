802b028967f2299a3d78f90e8a2d9b1083e4f7b6
Shuhib1924 <shuhib_s@live.de>
2024-08-13 21:55:45 +0200

    add bs4

--- blog1/views.py
+++ blog1/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Post
 from django.forms import ModelForm
 from django import forms
+from bs4 import BeautifulSoup
+import requests
 
 
 def index(request):
@@ -13,16 +15,17 @@ def index(request):
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ["url", "body"]
         labels = {"body": "Caption"}
         widgets = {
             "body": forms.Textarea(
                 attrs={
                     "rows": 3,
                     "placeholder": "Add caption...",
-                    "class": "font1 text-4xl rounded-lg",
+                    "class": "font1 text-4xl",
                 }
-            )
+            ),
+            "url": forms.TextInput(attrs={"placeholder": "add url..."}),
         }
 
 
@@ -31,6 +34,22 @@ def post_create(request):
     if request.method == "POST":
         form = PostCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+            website = requests.get(form.data["url"])
+            sourcecode = BeautifulSoup(website.text, "html.parser")
+            find_image = sourcecode.select(
+                'meta[content^="https://live.staticflickr.com/"]'
+            )
+            image = find_image[0]["content"]
+            post.image = image
+
+            find_title = sourcecode.select("h1.photo-title")
+            title = find_title[0].text.strip()
+            post.title = title
+
+            find_artist = sourcecode.select("a.owner-name")
+            artist = find_artist[0].text.strip()
+            post.artist = artist
+            post.save()
             return redirect("index")
     return render(request, "blog1/post_create.html", {"form": form})
