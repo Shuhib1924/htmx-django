d10c2bb82681bae1534a2e5d6327a1f8c53ab9f9
Shuhib1924 <shuhib_s@live.de>
2024-09-04 09:18:01 +0200

    todo1 add update & delete

--- todo1/views.py
+++ todo1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Todo
 from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse
 
 
 def index(request):
@@ -18,3 +19,20 @@ def add_todo(request):
         # todo = Todo.objects.create(title=title)
         todo.save()
     return render(request, "todo1/task.html", {"todo": todo})
+
+
+@require_http_methods(["PUT"])
+def update_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    # todo.is_done = True
+    todo.is_done = not todo.is_done
+    todo.save()
+    return render(request, "todo1/task.html", {"todo": todo})
+
+
+@require_http_methods(["DELETE"])
+def delete_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    todo.delete()
+    # return render(request, "todo1/index.html")
+    return HttpResponse(f"delete: {todo.title}")
