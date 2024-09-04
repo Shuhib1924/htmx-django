31bc43c9cec91b640b2577214c2b466efbbbaad9
Shuhib1924 <shuhib_s@live.de>
2024-09-04 08:00:56 +0200

    todo1 add task

--- todo1/views.py
+++ todo1/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Todo
+from django.views.decorators.http import require_http_methods
 
 
 def index(request):
-    return render(request, "todo1/index.html")
+    todos = Todo.objects.all()
+    return render(request, "todo1/index.html", {"todos": todos})
+
+
+@require_http_methods(["POST"])
+def add_todo(request):
+    print(request.POST)
+    todo = None
+    title = request.POST.get("title", "")
+    if title:
+        todo = Todo(title=title)
+        # todo = Todo.objects.create(title=title)
+        todo.save()
+    return render(request, "todo1/task.html", {"todo": todo})
