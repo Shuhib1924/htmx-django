798a1d47e5013d0d7f8f17b280e66138541d69d4
Shuhib1924 <shuhib_s@live.de>
2024-09-13 23:20:38 +0200

    create book

--- book2/views.py
+++ book2/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Book
 
 
 def index(request):
-    return render(request, "list1/index.html")
+    return render(request, "book2/index.html")
 
 
 def task(request):
-    return render(request, "list1/task.html")
+    if request.method == "POST":
+        print(request.POST)
+        get_title = request.POST.get("title")
+        get_author = request.POST.get("author")
+        Book.objects.create(title=get_title, author=get_author)
+        # book = Book(title=get_title, author=get_author)
+        # book.save()
+    return render(request, "book2/task.html")
