7824360f5fe01effe2b89cb73ff905a28b429719
Shuhib1924 <shuhib_s@live.de>
2024-09-14 06:03:02 +0200

    add and swap

--- book2/views.py
+++ book2/views.py
@@ -3,7 +3,7 @@ from .models import Book
 
 
 def index(request):
-    return render(request, "book2/index.html")
+    return render(request, "book2/index.html", {"books": Book.objects.all()})
 
 
 def task(request):
@@ -11,7 +11,7 @@ def task(request):
         print(request.POST)
         get_title = request.POST.get("title")
         get_author = request.POST.get("author")
-        Book.objects.create(title=get_title, author=get_author)
         # book = Book(title=get_title, author=get_author)
-        # book.save()
-    return render(request, "book2/task.html")
+        book = Book.objects.create(title=get_title, author=get_author)
+        book.save()
+    return render(request, "book2/task.html", {"book": book})
