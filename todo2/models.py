ffba1c7806cac2c348540b2555666b057f9d7bb5
Shuhib1924 <shuhib_s@live.de>
2024-09-15 13:24:17 +0200

    todo2: setup

--- todo2/models.py
+++ todo2/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tasks", blank=True, null=True
+    )
     title = models.CharField(max_length=100)
     status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
