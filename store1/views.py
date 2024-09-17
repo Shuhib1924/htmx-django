06a479d2512374bc389c7868619548a97610631d
Shuhib1924 <shuhib_s@live.de>
2024-09-17 11:20:31 +0200

    store1: add product & category

--- store1/views.py
+++ store1/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def index(request):
-    return render(request, "store1/index.html")
+    products = Product.objects.all()[0:8]
+    return render(request, "store1/index.html", {"products": products})
