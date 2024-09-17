ca4e290dc400fe199313d927a4038dfcbf186b64
Shuhib1924 <shuhib_s@live.de>
2024-09-17 13:55:23 +0200

    store1: shop + product_detail

--- store1/views.py
+++ store1/views.py
@@ -5,3 +5,12 @@ from .models import Product
 def index(request):
     products = Product.objects.all()[0:8]
     return render(request, "store1/index.html", {"products": products})
+
+
+def shop(request):
+    products = Product.objects.all()
+    return render(request, "store1/shop.html", {"products": products})
+
+
+def product(request):
+    return render(request, "store1/product.html")
