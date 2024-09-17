5b85448a192679dd7a744d2c016175bb03c749e8
Shuhib1924 <shuhib_s@live.de>
2024-09-17 14:14:54 +0200

    store1: add search

--- store1/views.py
+++ store1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
+from django.db.models import Q
 
 
 def index(request):
@@ -8,8 +9,27 @@ def index(request):
 
 
 def shop(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
-    return render(request, "store1/shop.html", {"products": products})
+    active_category = request.GET.get("category", "")
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get("query", "")
+
+    if query:
+        products = products.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+
+    context = {
+        "categories": categories,
+        "products": products,
+        "active_category": active_category,
+    }
+
+    return render(request, "store1/shop.html", context)
 
 
 def product(request):
