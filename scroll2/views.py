35ed65fe930e6ff467d6e48af0eb9345ba5a872b
Shuhib <shuhib_s@live.de>
2024-11-04 16:37:02 +0100

    scroll2: integrate htmx scroll / completed

--- scroll2/views.py
+++ scroll2/views.py
@@ -1,5 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, "scroll2/index.html")
+
+
+from django.views.generic import ListView
+
+from scroll2.models import Product
+
+
+class HomeView(ListView):
+    model = Product
+    template_name = "scroll2/index.html"
+    context_object_name = "products"
+    paginate_by = 10
